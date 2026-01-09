5f4b37208bf4f128d4e89311c05560771e6de378
Bhashit Gautam <bhashitgtm004@gmail.com>
2026-01-09 17:18:28 +0530

    Add function declarations: push, pop, print, reverse, free, get_size

--- vectors.h
+++ vectors.h
@@ -3,32 +3,22 @@
 
 #include <stdio.h>
 
-// All the supported data types
-// To allocate proper amount of memory 
-// we map enum member to type_size array (declared in vectors.c)
-enum DataTypes {
-  INT,
-  FLOAT,
-  LONG
-};
-
 typedef struct Vector {
-  enum DataTypes data_type;
   void* data;
   size_t size;
   size_t capacity;
+  size_t element_size;
 } Vector;
 
-Vector* init_vector(enum DataTypes data_type, size_t initial_capacity);
-void push_back(Vector* vector, ...);
-void print_vector(const Vector* vector);
-int get_element_at(const Vector* vector, size_t index, void* element);
+Vector* init_vector(size_t element_size, size_t initial_capacity);
+void push_back(Vector* vector, const void* element);
+void pop_back(Vector* vector, void* deleted_element);
+void print_vector(const Vector* vector, void (*print_element)(const void*));
+int get_element_at(const Vector* vector, size_t index, void* out_element);
 size_t get_size(const Vector* vector);
+void reverse_vector(Vector* vector);
 void free_vector(Vector* vector);
 
-// Add int_vector_pop_back
-// Add int_vector_reserve
 // Add int_vector_shrink_to_fit
-// Add errno-style error codes
 
 #endif
